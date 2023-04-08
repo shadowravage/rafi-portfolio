36480a00ca9bfb3ce4329de506a38731d52196f7
shadowravage <rafipatwary3@gmail.com>
2023-04-08 02:39:10 -0400

    Name change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Rafi's Portfolio Projects
         </p>
         <a
           className="App-link"
